a6b919d9a3cdcb44fc82936e1a8464302086b21f
Abdulrahaman Ridwan Ayinla <abdulrahamanr657@gmail.com>
2025-05-26 08:55:07 -0700

    Add: animation and hover style

--- script.js
+++ script.js
@@ -1,47 +1,80 @@
-'use strict';
+"use strict";
 
-const barCharts = [...document.querySelectorAll('.bar')];
-const amountEls = [...document.querySelectorAll('.amount')];
-const bars = [...document.querySelectorAll('.bar')]
+const barChartsDiv = document.getElementById('bar-chart-divs')
+const barCharts = [...document.querySelectorAll(".bar")];
+const amountEls = [...document.querySelectorAll(".amount")];
+const bars = [...document.querySelectorAll(".bar")];
 
-console.log(barCharts);
-console.log(amountEls);
+// console.log(barCharts);
+// console.log(amountEls);
 
-const formatcur = function(value, locale, currency) {
-        return new Intl.NumberFormat(locale, {
-      style: 'currency',
-      currency: currency
-    }).format(value)  
-}
+//Formatting the amount to a standard currency
+const formatcur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+};
 
-fetch('data.json')
-.then(response => response.json())
-.then(data =>{
+//fetching the data from the json file to work with
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
+
+    //getting the highest day with the most spending
+    const maxAmount = data.reduce((max, current) => {
+      return current.amount > max.amount ? current : max;
+    });
+
+    // console.log(maxAmount);
+
+    //displaying the bars base on the amount spend a day
     barCharts.forEach((bar, i) => {
-    const height = Math.round(3 * data[i].amount)
-    console.log(height);
-    bar.style.height = `${height}px`
-    amountEls[i].textContent = `${formatcur(Number(data[i].amount), 'en-US', 'USD')}`;
-    
-    
-    bar.addEventListener('mouseover', () => {
-        amountEls[i].style.display = 'block';
-    })
-    
-    bar.addEventListener('mouseout', () => {
-        amountEls[i].style.display = 'none';
-    })
-    
-    bars.forEach((b) => {
-        bar.addEventListener('click', () => {
-        amountEls[i].style.display = 'block';
-        console.log('clicked');
-    })
-    })
-    
-})
-})
-.catch(error => {
-    console.log('Error loading JSON', error)
-});
+      const height = Math.round(3 * data[i].amount);
+      //   console.log(height);
+    //   console.log(maxAmount.amount);
+      bar.style.height = `${height}px`;
+      if (height === Math.round(3 * maxAmount.amount)) {
+        bar.style.backgroundColor = "hsl(186, 34%, 65%)";
+        // console.log(maxAmount.amount);
+      }
+      
+      //displaying the amount on top of the bar chart
+      amountEls[i].textContent = `${formatcur(
+        Number(data[i].amount),
+        "en-US",
+        "USD"
+      )}`;
+
+      bar.addEventListener("mouseover", () => {
+        amountEls[i].style.display = "block";
+      });
+
+      bar.addEventListener("mouseout", () => {
+        amountEls[i].style.display = "none";
+      });
+    });
+  })
+  .catch((error) => {
+    console.log("Error loading JSON", error);
+  });
+
+//added a clock because why not???
+const timeEl = document.getElementById("time");
+// console.log(timeEl);
+
+const tick = function () {
+  const now = new Date();
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+
+  timeEl.textContent = new Intl.DateTimeFormat("en-US", options).format(now);
+};
+
+tick();
+setInterval(tick, 1000);
